acecde12615949990559ccf4cec2b62c5a2edbf1
EmreSafaCelik <safaemrecelik@gmail.com>
2024-02-25 21:53:48 +0300

    added version and memory

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ import subprocess
 def start(online_mode):
     command = f"sudo docker run --volume=./data:/data \
         -e EULA=TRUE \
-        -e ONLINE_MODE={online_mode} \
+        -e ONLINE_MODE={"TRUE" if online_mode else "FALSE"} \
+        -e VERSION={version} \
+        -e MEMORY={memory}G \
         itzg/minecraft-server -d"
     print(command)
     subprocess.run(command, shell=True)
 
 with gr.Blocks() as interface:
-    online_mode = gr.Dropdown(choices=["TRUE", "FALSE"], label="ONLINE MODE")
+    online_mode = gr.Checkbox(label="ONLINE MODE", info="Check it if the accounts are paid, don't if they are cracked.")
+    version = gr.Textbox(label="Version", info="e.g. 1.12.2, 1.20.4")
+    memory = gr.Slider(1, 128, value=4, label="Memory")
     start_btn = gr.Button("Start Server")
     start_btn.click(fn=start, inputs=online_mode, api_name="start")
 
