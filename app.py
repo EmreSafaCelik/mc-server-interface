68a72ab111ab39bf54953133daaac80c0170d480
EmreSafaCelik <safaemrecelik@gmail.com>
2024-02-25 19:56:09 +0300

    added EULA=TRUE to docker run

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def start(online_mode):
     > .env"""
     subprocess.run(command_env_file, shell=True)
 
-    command = f"sudo docker run --volume=./data:/data --env-file=.env itzg/minecraft-server -d"
+    command = f"sudo docker run --volume=./data:/data --env-file=.env -e EULA=TRUE itzg/minecraft-server -d"
     subprocess.run(command, shell=True)
 
 with gr.Blocks() as interface:
