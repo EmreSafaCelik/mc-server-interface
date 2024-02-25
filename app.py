ede439f779c2e082c430d8647a46272b5f492085
EmreSafaCelik <safaemrecelik@gmail.com>
2024-02-25 19:41:57 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import gradio as gr
+import subprocess
+
+def start(online_mode):
+    command_env_file = f"""echo ONLINE_MODE={online_mode}
+     > .env"""
+    subprocess.run(command_env_file, shell=True)
+
+    command = f"docker run --volume=./data:/data --env-file=.env itzg/minecraft-server -d"
+    subprocess.run(command, shell=True)
+
+with gr.Blocks() as interface:
+    online_mode = gr.Dropdown(choices=["TRUE", "FALSE"], label="ONLINE MODE")
+    start_btn = gr.Button("Start Server")
+    start_btn.click(fn=start, inputs=online_mode, api_name="start")
+
+interface.launch()
