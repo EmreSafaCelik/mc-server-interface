9c106f1aae72966ec8ea2308dcd5641852fe933f
EmreSafaCelik <safaemrecelik@gmail.com>
2024-02-25 21:59:43 +0300

    added .value to things

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ def start(online_mode):
     command = f"sudo docker run --volume=./data:/data \
         -e EULA=TRUE \
         -e ONLINE_MODE={'TRUE' if online_mode else 'FALSE'} \
-        -e VERSION={version} \
-        -e MEMORY={memory}G \
+        -e VERSION={version.value} \
+        -e MEMORY={memory.value}G \
         itzg/minecraft-server -d"
     print(command)
     subprocess.run(command, shell=True)
@@ -14,7 +14,7 @@ def start(online_mode):
 with gr.Blocks() as interface:
     online_mode = gr.Checkbox(label="ONLINE MODE", info="Check it if the accounts are paid, don't if they are cracked.")
     version = gr.Textbox(label="Version", info="e.g. 1.12.2, 1.20.4")
-    memory = gr.Slider(1, 128, value=4, label="Memory")
+    memory = gr.Slider(1, 64, value=4, label="Memory", info="How much RAM do you want the server to use?")
     start_btn = gr.Button("Start Server")
     start_btn.click(fn=start, inputs=online_mode, api_name="start")
 
